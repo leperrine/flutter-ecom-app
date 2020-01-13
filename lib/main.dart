074142a2aca408ab4d5627dbf910dc07cefee885
leperrine <leperrine@gmail.com>
2020-01-12 22:50:17 -0600

    updated add products screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import './providers/orders.dart';
 import './providers/cart.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
           UserProductsScreen.routeName: (context) => UserProductsScreen(),
+          EditProductScreen.routeName: (context) => EditProductScreen(),
         },
       ),
     );
